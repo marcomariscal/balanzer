d0b641c3d1d02679b44c8bf964d139d6648284b1
marco mariscal <marco.a.mariscal@gmail.com>
2020-10-12 20:00:02 -0400

    updating backend

--- backend/data.sql
+++ backend/data.sql
@@ -4,6 +4,3 @@ CREATE TABLE users(
     email TEXT,
     shrimpy_user_id TEXT NOT NULL UNIQUE
 );
-
--- INSERT INTO users (username, password, email, shrimpy_user_id) VALUES
---     ('demo', 'password', 'demo@gmail.com', 'b5a152de-0a3e-4e56-b189-379bb578c1f4');
